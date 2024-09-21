084244861dbe38138a9348e8ec90acd9442f8668
Miftahulislam690 <miftahulislam.info@gmail.com>
2024-09-21 21:00:14 +0600

    added more code

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -1,6 +1,14 @@
 document.getElementById('add-money-btn').addEventListener('click', function(event){
     event.preventDefault();
+
     const addMoney = getInputFieldValueById('input-add-amount');
     const pinNumber = getInputFieldValueById('input-pin-number');
-    console.log(addMoney, pinNumber);
+
+    if(pinNumber === 5555){
+        const balance = getTextFieldValueById('account-balance');
+        const totalBalance = balance + addMoney; 
+        document.getElementById('account-balance').innerText = totalBalance;
+    }else{
+        alert('Failed to add money');
+    }
 })
--- js/utiliti2.js
+++ js/utiliti2.js
@@ -3,3 +3,9 @@ function getInputFieldValueById(id){
     inputNumber = parseFloat(inputValue);
     return inputNumber;
 }
+
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    textValueNumber = parseFloat(textValue);
+    return textValueNumber;
+}
