f86c734c9aaae1b6d052751552e0a466850bfd26
Miftahulislam690 <miftahulislam.info@gmail.com>
2024-09-22 01:00:27 +0600

    added more code

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -1,14 +1,21 @@
 document.getElementById('add-money-btn').addEventListener('click', function(event){
     event.preventDefault();
-
     const addMoney = getInputFieldValueById('input-add-amount');
     const pinNumber = getInputFieldValueById('input-pin-number');
 
     if(pinNumber === 5555){
+        let count = 0;
+        count++;
         const balance = getTextFieldValueById('account-balance');
         const totalBalance = balance + addMoney; 
         document.getElementById('account-balance').innerText = totalBalance;
+
+        // add to transaction history
+        const p = document.createElement('p');
+        p.innerText = `Count ${count} Added ${addMoney} TK. ${totalBalance}`;
+        // console.log(p);
+        document.getElementById('transaction-section').appendChild(p);
     }else{
-        alert('Failed to add money');
+        alert('তোর জন্য কোনো টাকা পয়সা নাই, দূরে গিয়া মর ।।');
     }
 })
--- js/cashOut.js
+++ js/cashOut.js
@@ -7,6 +7,18 @@ document.getElementById('cash-out-btn').addEventListener('click', function(event
         const balance = getTextFieldValueById('account-balance');
         const newBalance = balance - cashOut;
         document.getElementById('account-balance').innerText = newBalance;
+
+        // add to transaction history
+        const div = document.createElement('div');
+        div.classList.add('bg-red-400');
+        div.innerHTML = `
+            <div class = 'mt-3 mb-4'>
+                <h3 class = 'font-bold text-xl text-slate-50'>Cash Out</h3>
+                <p class = 'text-slate-50'>${cashOut} Withdraw. New Balance ${newBalance}</p>
+            </div>
+        `
+
+        document.getElementById('transaction-section').appendChild(div);
     }else{
         alert('তোর জন্য কোনো টাকা পয়সা নাই, দূরে গিয়া মর ।।')
     }
